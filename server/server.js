51997d0a82bc0c94434b54cf9e3abb6f1ac558e5
FRedRick97 <chopra.aakash7@gmail.com>
2018-04-06 14:54:10 +0530

    no promise

--- server/server.js
+++ server/server.js
@@ -10,40 +10,39 @@ var app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
-    const express = require('express');
-    var todo = new Todo({
-        text: req.body.text
-    });
-    todo.save().then((doc) => {
-        res.send(doc);
-    }, (err) => {
-        res.status(400).send(err);
-    });
-});
+// app.post('/todos', (req, res) => {
+//     const express = require('express');
+//     var todo = new Todo({
+//         text: req.body.text
+//     });
+//     todo.save().then((doc) => {
+//         res.send(doc);
+//     }, (err) => {
+//         res.status(400).send(err);
+//     });
+// });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({ todos });
-    }, (err) => {
-        res.status(400).send(err);
+    Todo.find({}, function (err, todos) {
+        if (err) return err;
+        res.send(todos);
     });
 });
 
-app.get('/todos/:id', (req, res) => {
-    let id = req.params.id;
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
+// app.get('/todos/:id', (req, res) => {
+//     let id = req.params.id;
+//     if (!ObjectID.isValid(id)) {
+//         return res.status(404).send();
+//     }
 
-    Todo.findById(id).then((todo) => {
-        if (!todo)
-            return res.status(404).send();
-        res.send({ todo });
-    }).catch((err) => {
-        res.status(404).send(err);
-    });
-});
+//     Todo.findById(id).then((todo) => {
+//         if (!todo)
+//             return res.status(404).send();
+//         res.send({ todo });
+//     }).catch((err) => {
+//         res.status(404).send(err);
+//     });
+// });
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
