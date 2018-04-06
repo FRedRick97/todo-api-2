63419913063d219779d53ab5b415970bd0094947
FRedRick97 <chopra.aakash7@gmail.com>
2018-04-06 15:02:50 +0530

    added db authentication

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,4 +3,15 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
+
+mongoose.connect(process.env.MONGOLAB_URI, {
+    auth: {
+        user: 'FRedRick97',
+        password: 'admin_123#'
+    }
+})
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
+
+
 module.exports = mongoose;
